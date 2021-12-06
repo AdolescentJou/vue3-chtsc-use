e17d516b2d1066d4762ef6f2d0c0b71eb974d5c8
oujie01 <oujie01@huohua.cn>
2021-12-06 17:16:03 +0800

    'del[axioscod]'

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,21 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import TodoListDemo from "./TodoList/index";
-import {BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
-// import TsTest from "./tsUtilTest/tsRecordTest";
-// import AxiosTest from "./axiosTest/index";
-import DesignHookTest from "./reactTest/designHookTest";
-import  UseEffectTest from "./reactTest/useEffectTest"; 
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "antd/dist/antd.css";
 function App() {
   return (
     <Provider store={store}>
       <Router>
-      <Switch>
-        <Route path="/index" component={TodoListDemo} />
-        <Route path="/designHookTest" component={DesignHookTest} />
-        <Route path="/useEffectTest" component={UseEffectTest} />
-	<Redirect path="/*" to="/index"/>
-      </Switch>
+        <Switch>
+          <Route path="/index" component={TodoListDemo} />
+          <Redirect path="/*" to="/index" />
+        </Switch>
       </Router>
     </Provider>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,13 @@
 import { applyMiddleware, createStore } from "redux";
 import taskReducer, { state as taskState } from "../TodoList/reducer/reducer";
-import userReducer, { state as userState} from "../axiosTodoList/reducer/reducer";
 import thunk from "redux-thunk";
 import { combineReducers } from "redux";
 export interface storeType {
   taskReducer: taskState;
-  userReducer: userState;
 }
 
 const reducers = combineReducers({
   taskReducer,
-  userReducer
 });
 
 const store = createStore(reducers, applyMiddleware(thunk));
